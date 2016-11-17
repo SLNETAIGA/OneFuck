6a75d2dcc0faa50cfa8c6e4b4c2798c3eb31fb91
Andry Kvart <andry-kvart@web.de>
2016-11-17 12:24:00 +0400

    Add files via upload

---
+++ OneFuck.php
@@ -0,0 +1,63 @@
+<?php
+    // OneFuck.php
+    // Implement of OneFuck esoteric language(http://esolangs.org/wiki/OneFuck)
+	// Copyrigth SLNETAIGA
+	$source = file_get_contents($argv[1]);
+    $source = preg_replace("/\#.+?\n/","",$source,-1);
+	$source = str_split($source);
+	
+	
+	$cell = 0;
+	$brackets = 0;
+	for($i=0; $i<count($source); ++$i) {
+		switch($source[$i]) {
+			case "":break;
+			case " ":break;
+			case "\t":break;
+			case "\n":break;
+			case "+" :
+				$cell++;
+				break;
+			case "-" :
+				$cell--;
+				break;
+			case "." :
+				print chr($cell);
+				break;
+			case "`" :
+				print $cell;
+				break;
+			case "!" :
+				$cell = 0;
+				break;
+			case "," :
+				$cell = ord(fgetc(STDIN));
+				break;
+			case "[" :
+				if(!$chain[$cell]) {
+					$brackets = 1;
+					while($brackets) {
+						$i++;
+						if($source[$i] == "[") {
+							$brackets++;
+						} else if($source[$i] == "]") {
+							$brackets--;
+						}
+					}
+				}
+				break;
+			case "]" :
+				if($chain[$cell]) {
+					$brackets = 1;
+					while($brackets) {
+						$i--;
+						if($source[$i] == "]") {
+							$brackets++;
+						} else if($source[$i] == "[") {
+							$brackets--;
+						}
+					}
+				}
+				break;
+		}
+	}
